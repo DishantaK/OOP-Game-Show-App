503871b9d1fa39501a5f215d8fd21225a7801efe
DishantaK <nykiakpatrick@myself.com>
2021-07-19 12:01:06 -0400

    bug fix

--- js/app.js
+++ js/app.js
@@ -1,17 +1,15 @@
 /* Treehouse FSJS Techdegree
  * Project 4 - OOP Game App
  * app.js */
-
-
+let game;
 const startButton = document.getElementById('btn__reset');
-
 startButton.addEventListener('click', (e)=>{
 
 
     
     //
 
-    let game = new Game();
+    game = new Game();
     game.startGame();
     // console.log(`Active Phrase - phrase: ${game.activePhrase.phrase}`); //Leaving for Testing / grading 
     console.log("game", game);
